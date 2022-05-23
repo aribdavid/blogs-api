18e94d7332f2374bacb2100fc2c3d5d5a6a54f71
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-05-23 19:36:34 -0300

    feat: created postCategory model

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+  },
+  {
+    timestamps: false,
+  });
+
+  PostCategory.associate = ({ Category, BlogPost }) => {
+    Category.belongsToMany(BlogPost, {
+      as: 'blogPosts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+      onDelete: 'CASCADE',
+    });
+
+    BlogPost.belongsToMany(Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+      onDelete: 'CASCADE',
+    });
+  }
+
+  return PostCategory;
+} 
