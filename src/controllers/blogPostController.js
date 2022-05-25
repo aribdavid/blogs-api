f609b30a8f7c65c690ca4e2c3cf9bdc283345eb2
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-05-25 14:26:23 -0300

    feat: created getAll method from blogPost controller layer

--- src/controllers/blogPostController.js
+++ src/controllers/blogPostController.js
@@ -5,9 +5,17 @@ const createPost = async (request, response) => {
   const { title, content, categoryIds } = request.body;
 
   const newPost = await blogPostService.createPost(decodedData, title, content, categoryIds);
+  
   return response.status(201).json(newPost);
+};
+
+const getAll = async (_request, response) => {
+  const posts = await blogPostService.getAll();
+  
+  return response.status(201).json(posts);
 };
 
 module.exports = {
   createPost,
+  getAll,
 };
